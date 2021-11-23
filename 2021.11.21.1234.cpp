54b5cabaab59b8259e04b648fbae1592cba2db40
yolo <77111763+yoloeww@users.noreply.github.com>
2021-11-23 08:06:02 +0800

    Update 2021.11.21.1234.cpp

--- 2021.11.21.1234.cpp
+++ 2021.11.21.1234.cpp
@@ -1,16 +1,17 @@
 # include<stdio.h>
+# include<string.h>
 int main(void)
 {
-  char a[8],i;
+  char a[10],i;
   printf("input array:\n");
-  for(i=0;i<4;i++)
-	  scanf("%c",&a[i]);
-  for(i=4;i>=0;i--)
+  scanf("%s",a);
+  int k=strlen(a);
+  for(i=k;i>0;i--)
   {
-    a[i*2-1]=a[i];
-	a[i]=' ';
-  }
-  for(i=0;i<8;i++)
-	  printf("%c",a[i]);
+    a[i*2]=a[i];
+
+	a[i*2-1]=' ';
+}	  
+  printf("%s",a);
   return 0;
 }
