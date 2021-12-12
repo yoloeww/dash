9aa23b7646b49f0a72c922444c92e9033557c09f
yolo <77111763+yoloeww@users.noreply.github.com>
2021-12-12 23:52:24 +0800

    Add files via upload

---
+++ 2021.12.12.一串.cpp
@@ -0,0 +1,18 @@
+# include<stdio.h>
+# include<string.h>
+int main(void)
+{
+   int a[20],len,i,s;
+   char str[100];
+   printf("input string:\n");
+   gets(str);
+   len=strlen(str);
+   for(i=0;i<len;i++)
+   {
+       if(str[i]>='0'&&str[i]<='9')
+	   {
+	       s=str[i]-'0';
+	   }
+	   for(j=i+1;j<)
+   }
+} 
